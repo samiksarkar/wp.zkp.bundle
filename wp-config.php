634d88cfcc1fe85e5702e049288aa7b61fd3f4b0
Himanshu <himanshu.verma@zakoopi.com>
2015-09-16 19:38:40 +0530

    db

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', '{{dbname}}');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '{{dbuser}}');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '{{dbpass}}');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
